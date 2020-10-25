12197ceb6531bc20b0ab44006c55bee0fadd1f38
inc0der <inc0der.code@gmail.com>
2020-10-25 09:34:53 -0300

    Update object toolbar style (#34)
    * Refactor to use the label tag
    
    * Make filepath text small
    
    * Refactor method names
    
    * Refactor to use htmlFor and change remaining method names

--- app/js/renderer/components/toolbarProperties.jsx
+++ app/js/renderer/components/toolbarProperties.jsx
@@ -183,23 +183,23 @@ export default class ToolbarProperties extends React.Component {
     // TODO make each block a seperate component?
     return (
       <div className="propsContainer">
-        {this.block1(name)}
-        {this.block2(x, y, z)}
-        {this.block3(scaleX, scaleY, angle)}
-        {!isQSprite && this.block4(anchorX, anchorY)}
-        {this.block5(filePath, type, pose, isQSprite)}
-        {!isQSprite && this.block6(type, cols, rows, index, speed)}
-        {this.block7(conditions)}
-        {this.block8(notes)}
+        {this.nameBlock(name)}
+        {this.positionBlock(x, y, z)}
+        {this.transformBlock(scaleX, scaleY, angle)}
+        {!isQSprite && this.anchorBlock(anchorX, anchorY)}
+        {this.imageBlock(filePath, type, pose, isQSprite)}
+        {!isQSprite && this.imageGridBlock(type, cols, rows, index, speed)}
+        {this.conditionsBlock(conditions)}
+        {this.notesBlock(notes)}
         {/*this.block9(meta)*/}
       </div>
     );
   }
-  block1(name) {
+  nameBlock(name) {
     return (
       <div className="props">
-        <div className="full">Name</div>
         <div className="full">
+          <label htmlFor="text">Name</label>
           <input
             type="text"
             onChange={this.onChange}
@@ -210,13 +210,11 @@ export default class ToolbarProperties extends React.Component {
       </div>
     );
   }
-  block2(x, y, z) {
+  positionBlock(x, y, z) {
     return (
       <div className="props">
-        <div className="third">x</div>
-        <div className="third">y</div>
-        <div className="third">z</div>
         <div className="third">
+          <label htmlFor="x">x</label>
           <input
             type="text"
             onChange={this.onChange}
@@ -226,6 +224,7 @@ export default class ToolbarProperties extends React.Component {
           />
         </div>
         <div className="third">
+          <label htmlFor="y">y</label>
           <input
             type="text"
             onChange={this.onChange}
@@ -235,6 +234,7 @@ export default class ToolbarProperties extends React.Component {
           />
         </div>
         <div className="third">
+          <label htmlFor="z">z</label>
           <input
             type="text"
             onChange={this.onChange}
@@ -246,13 +246,11 @@ export default class ToolbarProperties extends React.Component {
       </div>
     );
   }
-  block3(scaleX, scaleY, angle) {
+  transformBlock(scaleX, scaleY, angle) {
     return (
       <div className="props">
-        <div className="third">ScaleX</div>
-        <div className="third">ScaleY</div>
-        <div className="third">Angle</div>
         <div className="third">
+          <label htmlFor="scaleX">ScaleX</label>
           <input
             type="text"
             onChange={this.onChange}
@@ -262,6 +260,7 @@ export default class ToolbarProperties extends React.Component {
           />
         </div>
         <div className="third">
+          <label htmlFor="scaleY">ScaleY</label>
           <input
             type="text"
             onChange={this.onChange}
@@ -271,6 +270,7 @@ export default class ToolbarProperties extends React.Component {
           />
         </div>
         <div className="third">
+          <label htmlFor="angle">Angle</label>
           <input
             type="text"
             onChange={this.onChange}
@@ -282,12 +282,11 @@ export default class ToolbarProperties extends React.Component {
       </div>
     );
   }
-  block4(anchorX, anchorY) {
+  anchorBlock(anchorX, anchorY) {
     return (
       <div className="props">
-        <div className="half">Anchor X</div>
-        <div className="half">Anchor Y</div>
         <div className="half">
+          <label htmlFor="anchorX">AnchorX</label>
           <input
             type="text"
             onChange={this.onChange}
@@ -297,6 +296,7 @@ export default class ToolbarProperties extends React.Component {
           />
         </div>
         <div className="half">
+          <label htmlFor="anchorY">AnchorY</label>
           <input
             type="text"
             onChange={this.onChange}
@@ -308,19 +308,21 @@ export default class ToolbarProperties extends React.Component {
       </div>
     );
   }
-  block5(filePath, type, pose, isQSprite) {
+  imageBlock(filePath, type, pose, isQSprite) {
     if (isQSprite) {
       return this.block5B(filePath, pose, isQSprite);
     }
     return (
       <div className="props">
-        <div className="half">Image</div>
-        <div className="half">Type</div>
         <div className="half">
-          <button onClick={this.openFile}>Select File</button>
-          {filePath}
+          <label htmlFor="image">Image</label>
+          <button name="image" onClick={this.openFile}>
+            Select File
+          </button>
+          <small>{filePath}</small>
         </div>
         <div className="half">
+          <label htmlFor="type">Type</label>
           <select value={type} onChange={this.onChange} name="type">
             <option value="full">Full</option>
             <option value="spritesheet">SpriteSheet</option>
@@ -330,7 +332,7 @@ export default class ToolbarProperties extends React.Component {
       </div>
     );
   }
-  block5B(filePath, pose, isQSprite) {
+  qSpriteBlock(filePath, pose, isQSprite) {
     const { poses } = Store.getQSprite(isQSprite);
     let list = [];
     for (let pose in poses) {
@@ -344,13 +346,15 @@ export default class ToolbarProperties extends React.Component {
     }
     return (
       <div className="props">
-        <div className="half">Image</div>
-        <div className="half">Pose</div>
         <div className="half">
-          <button onClick={this.openFile}>Select File</button>
+          <label htmlFor="image">Image</label>
+          <button name="image" onClick={this.openFile}>
+            Select File
+          </button>
           {filePath}
         </div>
         <div className="half">
+          <label htmlFor="pose">Pose</label>
           <select value={pose} onChange={this.onChange} name="pose">
             <option value=""></option>
             {list}
@@ -359,15 +363,12 @@ export default class ToolbarProperties extends React.Component {
       </div>
     );
   }
-  block6(type, cols, rows, index, speed) {
+  imageGridBlock(type, cols, rows, index, speed) {
     if (type !== "spritesheet" && type !== "animated") return null;
     return (
       <div className="props">
-        <div className="third">Cols</div>
-        <div className="third">Rows</div>
-        {type === "spritesheet" && <div className="third">Index</div>}
-        {type === "animated" && <div className="third">Speed</div>}
         <div className="third">
+          <label htmlFor="cols">Cols</label>
           <input
             type="text"
             onChange={this.onChange}
@@ -377,6 +378,7 @@ export default class ToolbarProperties extends React.Component {
           />
         </div>
         <div className="third">
+          <label htmlFor="rows">Rows</label>
           <input
             type="text"
             onChange={this.onChange}
@@ -387,11 +389,15 @@ export default class ToolbarProperties extends React.Component {
         </div>
         {type === "spritesheet" && (
           <div className="third">
-            <button onClick={this.openSelectIndex}>Select</button>
+            <label htmlFor="frame">Frame</label>
+            <button name="frame" onClick={this.openSelectIndex}>
+              Select
+            </button>
           </div>
         )}
         {type === "animated" && (
           <div className="third">
+            <label htmlFor="speed">Speed</label>
             <input
               type="text"
               onChange={this.onChange}
@@ -403,7 +409,7 @@ export default class ToolbarProperties extends React.Component {
       </div>
     );
   }
-  block7(conditions) {
+  conditionsBlock(conditions) {
     return (
       <div className="props">
         <div className="full">Conditions</div>
@@ -437,22 +443,22 @@ export default class ToolbarProperties extends React.Component {
       </div>
     );
   }
-  block8(notes) {
+  notesBlock(notes) {
     return (
       <div className="props">
-        <div className="full">Notes</div>
         <div className="full">
+          <label htmlFor="notes">Notes</label>
           <textarea onChange={this.onChange} name="notes" value={notes} />
         </div>
       </div>
     );
   }
-  block9(meta) {
+  metaBlock(meta) {
     return (
       <div className="props">
-        <div className="full">Extras</div>
         <div className="full">
-          <button onClick={this.addExtra}>
+          <label htmlFor="extras">Extras</label>
+          <button name="extras" onClick={this.addExtra}>
             <i className="fa fa-plus" aria-hidden />
             New
           </button>
